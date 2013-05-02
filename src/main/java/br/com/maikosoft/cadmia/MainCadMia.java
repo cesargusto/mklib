dd833f80e375b00601dba38046eaec5bf38967a0
michael <michael@maikosoft.com.br>
2013-05-02 19:43:42 +0000

    iniciando migaracao projeto otica alianca

---
+++ src/main/java/br/com/maikosoft/cadmia/MainCadMia.java
@@ -0,0 +1,22 @@
+package br.com.maikosoft.cadmia;
+
+import br.com.maikosoft.cadmia.view.JanelaClienteConsultaRapida;
+import br.com.maikosoft.cadmia.view.JanelaLogin;
+import br.com.maikosoft.mklib.MkApplication;
+
+public class MainCadMia {
+
+	public static void main(String[] args) {
+		
+		MkApplication application = MkApplication.getInstance();
+		
+		application.init("CadMia", "cadmia");
+		
+		JanelaLogin.getInstance();
+		
+		application.updateMkMenu(EnumMenu.getListMenus());
+		
+		new JanelaClienteConsultaRapida().showWindow();
+	}
+
+}
